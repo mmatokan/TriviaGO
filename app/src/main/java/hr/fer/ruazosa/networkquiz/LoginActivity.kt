52c257422d0816dc0e4365960dde3e19964d82e0
fbosnjak <fabijan.bosnjak7@gmail.com>
2020-12-29 20:38:41 +0100

    pokusaj commita

---
+++ app/src/main/java/hr/fer/ruazosa/networkquiz/LoginActivity.kt
@@ -0,0 +1,17 @@
+package hr.fer.ruazosa.networkquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        loginButton.setOnClickListener {
+            var emailAddress: String? = emailEditText.text.toString()
+
+        }
+    }
+}
