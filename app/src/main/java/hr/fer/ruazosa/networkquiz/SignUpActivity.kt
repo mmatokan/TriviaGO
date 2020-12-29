c49b953af7d51ddf7aa72382fd8bcd5e117f1d4c
unknown <izabela.simic@fer.hr>
2020-12-29 21:27:07 +0100

    UI registracije

---
+++ app/src/main/java/hr/fer/ruazosa/networkquiz/SignUpActivity.kt
@@ -0,0 +1,14 @@
+package hr.fer.ruazosa.networkquiz
+
+import android.os.Bundle
+//import android.support.wearable.activity.WearableActivity
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+    }
+}
