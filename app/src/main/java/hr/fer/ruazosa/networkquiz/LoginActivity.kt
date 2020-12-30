c91c6d4e284d34edc6a1b3f8afd3b2f1321d9b1b
fbosnjak <fabijan.bosnjak7@gmail.com>
2020-12-30 01:13:37 +0100

    prepravke u kodu za login

--- app/src/main/java/hr/fer/ruazosa/networkquiz/LoginActivity.kt
+++ app/src/main/java/hr/fer/ruazosa/networkquiz/LoginActivity.kt
@@ -13,53 +13,8 @@ class LoginActivity : AppCompatActivity() {
 
         loginButton.setOnClickListener {
             var emailAddress: String = emailEditText.text.toString()
-            var beforeEt: Boolean = false
-            var containsEt: Boolean = false
-            var betweenEtAndDot: Boolean = false
-            var containsDot: Boolean = false
-            var afterDot: Boolean = false
-            for (c in emailAddress) {
-                if (beforeEt == true) {
-                    if (containsEt == true) {
-                        if (c == '@') break
-                        if (betweenEtAndDot == true) {
-                            if (containsDot == true) {
-                                if (afterDot == true) {
-                                    break
-                                } else {
-                                    if (c != '.')
-                                        afterDot = true
-                                    else
-                                        break
-                                }
-                            } else {
-                                if (c == '.')
-                                    containsDot = true
-                            }
-                        } else {
-                            if (c == '.')
-                                break
-                            else
-                                betweenEtAndDot = true
-                        }
-                    } else {
-                        if (c == '@')
-                            containsEt = true
-                    }
-                } else {
-                    if (c in 'a'..'z' || c in 'A'..'Z' || c in '0'..'9')
-                        beforeEt = true
-                    else
-                        break
-                }
-            }
-            if (beforeEt == true) println(message = "beforeEt je true") else println(message = "beforeEt je false")
-            if (containsEt == true) println(message = "containsEt je true") else println(message = "containsEt je false")
-            if (betweenEtAndDot == true) println(message = "between je true") else println(message = "between je false")
-            if (containsDot == true) println(message = "containsDot je true") else println(message = "containsDot je false")
-
-            if (afterDot == false) {
-                Toast.makeText(this, "Email address is not valid", Toast.LENGTH_LONG).show()
+            if(!android.util.Patterns.EMAIL_ADDRESS.matcher(emailAddress).matches())
+                Toast.makeText(this, "Invalid e-mail format", Toast.LENGTH_LONG).show()
             }
         }
         signUpButton.setOnClickListener{
