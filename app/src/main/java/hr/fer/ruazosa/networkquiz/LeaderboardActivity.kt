c3b7afad49bd39d95042e20827eff49523e27d08
fbosnjak <fabijan.bosnjak7@gmail.com>
2021-01-14 00:11:57 +0100

    zavrseni leaderboard activity i adapter, ne znam jel radi jer ne mogu pokrenut backend jos uvijek, ima jedan error pri pozivanju rest.getLeaderboard() u LeaderboardActivity pa ako netko zna u cem je stvar, super

--- app/src/main/java/hr/fer/ruazosa/networkquiz/LeaderboardActivity.kt
+++ app/src/main/java/hr/fer/ruazosa/networkquiz/LeaderboardActivity.kt
@@ -18,16 +18,50 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.gson.Gson
 import hr.fer.ruazosa.networkquiz.entity.*
 import hr.fer.ruazosa.networkquiz.net.RestFactory
+import kotlinx.android.synthetic.main.activity_choose_players.*
 import kotlinx.android.synthetic.main.activity_leaderboard.*
+import kotlinx.android.synthetic.main.activity_leaderboard.returnButton
 
 class LeaderboardActivity : AppCompatActivity() {
     lateinit var adapter : LeaderboardUserAdapter
-    lateinit var data : MutableList<RankUser>
+    lateinit var data : MutableList<User>
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_leaderboard)
 
+        data = mutableListOf<User>()
+        GetLeaderboard().execute()
 
+        returnButton.setOnClickListener {
+            onBackPressed()
+            finish()
+        }
+
+        leaderboardUsersRecyclerView.layoutManager = LinearLayoutManager(application)
+        adapter = LeaderboardUserAdapter(data)
+        leaderboardUsersRecyclerView.adapter = adapter
+        val decorator = DividerItemDecoration(applicationContext, LinearLayoutManager.VERTICAL)
+        decorator.setDrawable(
+            ContextCompat.getDrawable(
+                applicationContext,
+                R.drawable.decorator1
+            )!!
+        )
+        leaderboardUsersRecyclerView.addItemDecoration(decorator)
+    }
+
+    private inner class GetLeaderboard:AsyncTask<String, Void, MutableList<User>?>(){
+
+        override fun doInBackground(vararg usernameToExclude: String): MutableList<User>? {
+            val rest = RestFactory.instance
+            return rest.getLeaderboard()
+        }
+
+        override fun onPostExecute(users: MutableList<User>?) {
+            if (users != null) {
+                data = users
+            }
+        }
     }
 }
--- app/src/main/java/hr/fer/ruazosa/networkquiz/LeaderboardUserAdapter.kt
+++ app/src/main/java/hr/fer/ruazosa/networkquiz/LeaderboardUserAdapter.kt
@@ -1,4 +1,48 @@
 package hr.fer.ruazosa.networkquiz
 
-class LeaderboardUserAdapter {
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import hr.fer.ruazosa.networkquiz.entity.User
+
+class LeaderboardUserAdapter (val data: MutableList<User>) : RecyclerView.Adapter<LeaderboardUserAdapter.ViewHolder>() {
+    private val TYPE_TOP3=0
+    private val TYPE_NOT_TOP3=1
+
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var rankTextView: TextView = view.findViewById(R.id.rankTextView)
+        var usernameTextView : TextView = view.findViewById(R.id.usernameLeaderboardTextView)
+        var scoreTextView: TextView = view.findViewById(R.id.scoreTextView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LeaderboardUserAdapter.ViewHolder {
+        var layout : Int? =  null
+        if (viewType == TYPE_TOP3){ layout = R.layout.leaderboard_item_top3 }
+        else { layout = R.layout.leaderboard_item_not_top3 }
+
+        val view = LayoutInflater.from(parent.context).inflate(layout, parent , false )
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: LeaderboardUserAdapter.ViewHolder, position: Int) {
+        holder.usernameTextView.text = data[position].username
+        holder.rankTextView.text = data[position].rank.toString()
+        holder.scoreTextView.text = data[position].score.toString()
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        if (data.get(position).rank <= 3){
+            return TYPE_TOP3
+        } else
+            return TYPE_NOT_TOP3
+    }
 }
